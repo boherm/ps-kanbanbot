4678b21fd62c1ea1e8f221bfa393ad4f533672f6
lartist <salim-benouamer.ext@prestashop.com>
2023-05-18 18:11:56 +0200

    Replace services.yaml by services.php

---
+++ config/services.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\PullRequest\Domain\Gateway\PRRepositoryInterface;
+use App\Infrastructure\Adapter\RestPRRepository;
+use App\Infrastructure\Adapter\SpyEventDispatcher;
+use App\Infrastructure\Adapter\SpyMessageBus;
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+return function (ContainerConfigurator $configurator) {
+
+    $services = $configurator->services();
+    $services->defaults()
+        ->autowire(true)
+        ->autoconfigure(true);
+
+    $services->load('App\\', '../src/')
+        ->exclude([
+            '../src/DependencyInjection/',
+            '../src/Entity/',
+            '../src/Kernel.php',
+        ]);
+
+    $services->load('App\\PullRequest\\Application\\CommandHandler\\', '../src/PullRequest/Application/CommandHandler/')
+        ->tag('messenger.message_handler');
+
+    $services->alias(PRRepositoryInterface::class, RestPRRepository::class);
+
+    if ('test' === $configurator->env()) {
+        $configurator->parameters()
+            ->set('test_tmp_dir', '%kernel.project_dir%/var/tests/tmp');
+
+        $services
+            ->defaults()
+                ->public()
+                ->autowire()
+        ;
+
+        $services->set(RestPRRepository::class);
+        $services->alias(MessageBusInterface::class, SpyMessageBus::class);
+    }
+};
