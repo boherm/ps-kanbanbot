23aa6fd90a60b168b5c6c047d2ffa43c33f5bc9b
boherm <boris.hermans@prestashop.com>
2024-04-09 09:30:37 +0200

    Add check new translations strings into strategies

--- src/Shared/Infrastructure/Factory/CommandFactory/Strategy/Command/PullRequestReadyForReviewStrategy.php
+++ src/Shared/Infrastructure/Factory/CommandFactory/Strategy/Command/PullRequestReadyForReviewStrategy.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Shared\Infrastructure\Factory\CommandFactory\Strategy\Command;
 
+use App\PullRequest\Application\Command\CheckTranslationsCommand;
 use App\PullRequestDashboard\Application\Command\MovePullRequestCardToColumnByLabelCommand;
 use App\Shared\Infrastructure\Factory\CommandFactory\CommandStrategyInterface;
 
@@ -52,17 +53,26 @@ class PullRequestReadyForReviewStrategy implements CommandStrategyInterface
      *     },
      * } $payload
      *
-     * @return array<MovePullRequestCardToColumnByLabelCommand>
+     * @return array<MovePullRequestCardToColumnByLabelCommand|CheckTranslationsCommand>
      */
     public function createCommandsFromPayload(array $payload): array
     {
+        $repoOwner = $payload['pull_request']['base']['repo']['owner']['login'];
+        $repoName = $payload['pull_request']['base']['repo']['name'];
+        $prNumber = (string) $payload['pull_request']['number'];
+
         return [
             new MovePullRequestCardToColumnByLabelCommand(
                 $this->pullRequestDashboardNumber,
-                $payload['pull_request']['base']['repo']['owner']['login'],
-                $payload['pull_request']['base']['repo']['name'],
-                (string) $payload['pull_request']['number'],
+                $repoOwner,
+                $repoName,
+                $prNumber,
                 $this->readyForReviewColumnName,
+            ),
+            new CheckTranslationsCommand(
+                $repoOwner,
+                $repoName,
+                $prNumber,
             ),
         ];
     }
